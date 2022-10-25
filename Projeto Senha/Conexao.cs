1de7fb69aefd020753d617097f431ae73322b2c3
aluno <aluno@LAB06-16>
2022-10-24 22:32:56 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto Senha/Conexao.cs
@@ -0,0 +1,47 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto_Senha
+{
+    public class Conexao
+    {
+        public MySqlConnection conecta;
+
+        public void Conectar()
+        {
+            if (conecta.State == ConnectionState.Closed)
+            {
+                conecta.ConnectionString = "Server=localhost;Database=bd_senha;User=root;Pwd=cursoads";
+                conecta.Open();
+            }
+        }
+        public void Desconectar()
+        {
+            if(conecta.State == ConnectionState.Open)
+            {
+                conecta.Close();
+            }
+        }
+        public int ExecSql(String sql)
+        {
+            int nlinhas;
+            try
+            {
+                MySqlCommand comando = new MySqlCommand(); //instacnia e recebe
+                comando.CommandText = sql; //recebe string de conexão
+                comando.Connection = conecta; //recebe a parte da conexão (status aberto, fechado)
+                nlinhas = comando.ExecuteNonQuery(); //execução comandos que n traz em tabela (select n entra)
+            } catch(Exception ex) //trabalha tratamentos = Exception (se der errado no codigo anterior)
+            {
+                Console.WriteLine("Erro: " + ex);
+                nlinhas = 0;
+            }
+            return nlinhas; //retorna 
+        }
+    }
+}
---
+++ Projeto Senha/Form1.Designer.cs
@@ -0,0 +1,82 @@
+﻿namespace Projeto_Senha
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNormal = new System.Windows.Forms.Button();
+            this.btnPreferencial = new System.Windows.Forms.Button();
+            this.lbSenha = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnNormal
+            // 
+            this.btnNormal.Location = new System.Drawing.Point(65, 31);
+            this.btnNormal.Name = "btnNormal";
+            this.btnNormal.Size = new System.Drawing.Size(122, 66);
+            this.btnNormal.TabIndex = 0;
+            this.btnNormal.Text = "Normal";
+            this.btnNormal.UseVisualStyleBackColor = true;
+            // 
+            // btnPreferencial
+            // 
+            this.btnPreferencial.Location = new System.Drawing.Point(65, 150);
+            this.btnPreferencial.Name = "btnPreferencial";
+            this.btnPreferencial.Size = new System.Drawing.Size(122, 66);
+            this.btnPreferencial.TabIndex = 1;
+            this.btnPreferencial.Text = "Preferencial";
+            this.btnPreferencial.UseVisualStyleBackColor = true;
+            // 
+            // lbSenha
+            // 
+            this.lbSenha.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.lbSenha.Location = new System.Drawing.Point(449, 31);
+            this.lbSenha.Name = "lbSenha";
+            this.lbSenha.Size = new System.Drawing.Size(212, 185);
+            this.lbSenha.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lbSenha);
+            this.Controls.Add(this.btnPreferencial);
+            this.Controls.Add(this.btnNormal);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btnNormal;
+        private Button btnPreferencial;
+        private Label lbSenha;
+    }
+}
